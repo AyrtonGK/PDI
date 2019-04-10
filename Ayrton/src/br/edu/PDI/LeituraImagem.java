fc4dcc7f0eafafde5e07d2c4c4810d3d60e30c14
THX96 <ayrton_gk@hotmail.com>
2019-04-10 14:42:55 -0300

    First commit

---
+++ Ayrton/src/br/edu/PDI/LeituraImagem.java
@@ -0,0 +1,20 @@
+package br.edu.PDI;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class LeituraImagem {
+
+	public void jButton1ActionPerformed() {
+		BufferedImage image;
+
+		String str = "D:\\Ayrton\\Trabalhos\\PDI\\images.jpg";
+		try {
+			image = ImageIO.read(new File(str));
+		} catch (IOException ex) {
+		}
+	}
+}
---
+++ Ayrton/src/br/edu/PDI/Principal.java
@@ -0,0 +1,11 @@
+package br.edu.PDI;
+
+import br.edu.PDI.LeituraImagem;
+public class Principal {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    LeituraImagem leitura = new LeituraImagem();
+    leitura.jButton1ActionPerformed();
+	}
+
+}
