4eb84bad520ed46509aef50b29ed4f2493817451
THX96 <ayrton_gk@hotmail.com>
2019-04-18 14:12:33 -0300

    removido os info e criadoo a funcao fft

--- Ayrton/src/br/edu/PDI/Principal.java
+++ Ayrton/src/br/edu/PDI/Principal.java
@@ -1,11 +1,54 @@
 package br.edu.PDI;
 
-import br.edu.PDI.info2DFFTdois;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Principal {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-    info2DFFTdois leitura = new info2DFFTdois();
-    leitura.jButton1ActionPerformed();
+
+		BufferedImage img = readFile(new File("Origem.PNG"));
+		int[][] matriz = img2mat(img);
+
+		System.out.print(matriz.length + "-" + img.getHeight());
 	}
 
+	public static BufferedImage readFile(File arquivo) {
+		BufferedImage buffImg = null;
+
+		try {
+			buffImg = ImageIO.read(arquivo);
+		} catch (IOException e) {
+		}
+
+		return buffImg;
+	}
+
+	public static int[][] img2mat(BufferedImage img) {
+
+		int height = img.getHeight();
+		int width = img.getWidth();
+
+		height = proximaPotencia2(height);
+		width = proximaPotencia2(width);
+
+		int[][] matriz = new int[height][width];
+
+		for (int i = 0; i < height; i++)
+			for (int j = 0; j < width; j++)
+				matriz[i][j] = 0;
+
+		for (int i = 0; i < img.getHeight(); i++)
+			for (int j = 0; j < img.getWidth(); j++)
+				matriz[i][j] = img.getRGB(j, i);
+
+		return matriz;
+	}
+
+	public static int proximaPotencia2(int numero) {
+		int highestOneBit = Integer.highestOneBit(numero);
+		return (numero == highestOneBit ? numero : highestOneBit << 1);
+	}
 }
--- Ayrton/src/br/edu/PDI/TwoD_FFT.java
+++ Ayrton/src/br/edu/PDI/TwoD_FFT.java
@@ -1,221 +1,119 @@
 package br.edu.PDI;
- 
-public class TwoD_FFT 
-{
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+
+import org.opencv.imgproc.*;
+
+public class TwoD_FFT {
 	/**
-	   this clsas provides the methods for the 2D-Fourier transformation
+	 * this clsas provides the methods for the 2D-Fourier transformation
+	 * 
+	 * @param amplitutude
 	 */
+	static void twoDDft(double[][] inputData, double[][] realOut,
 
-	public class Fourier2D {
+			double[][] complexOut, double[][] amplitudeOut) {
+		int height = inputData.length;
+		int width = inputData[0].length;
 
-	    /** size of matrix in M-direction */
-	    private int M_2P;
+		// Two outer loops iterate on output data.
+		for (int yWave = 0; yWave < height; yWave++) {
+			for (int xWave = 0; xWave < width; xWave++) {
+				// Two inner loops iterate on input data.
+				for (int ySpace = 0; ySpace < height; ySpace++) {
+					for (int xSpace = 0; xSpace < width; xSpace++) {
+						// Compute real, imag, and ampltude.
+						realOut[yWave][xWave] += (inputData[ySpace][xSpace] * Math
+								.cos(2 * Math.PI * ((1.0 * xWave * xSpace / width) + (1.0 * yWave * ySpace / height))))
+								/ Math.sqrt(width * height);
+						complexOut[yWave][xWave] -= (inputData[ySpace][xSpace] * Math
+								.sin(2 * Math.PI * ((1.0 * xWave * xSpace / width) + (1.0 * yWave * ySpace / height))))
+								/ Math.sqrt(width * height);
+						amplitudeOut[yWave][xWave] = Math.sqrt(realOut[yWave][xWave] * realOut[yWave][xWave]
+								+ complexOut[yWave][xWave] * complexOut[yWave][xWave]);
+					}
+					System.out.println(amplitudeOut[yWave][xWave]);
 
-	    /** size of matrix in N-direction  */
-	    private int N_2P;
-
-	    /** largest exponent, so that 2^maxPotM <= (M_2P-1) */
-	    private int maxPotM;
-
-	    /** largest exponent, so that 2^maxPotN <= (N_2P-1) */
-	    private int maxPotN;
-
-
-	    /** constructor for the use of the 2D transformation routines */ 
-	    public Fourier2D(int M, int N) {
-	        int M_tmp, N_tmp;
-
-	        // check for power of 2
-	        M_tmp = M;
-	        N_tmp = N;
-	        while( ((M_tmp >>= 1) != 0) && ((N_tmp >>= 1) != 0) ) {
-	            if( ((M_tmp & 0x1) == 0x1) && (M_tmp>>1 != 0) &&
-	                ((N_tmp & 0x1) == 0x1) && (N_tmp>>1 != 0) ) {
-	                System.out.println("Fourier: both dimensions must be powers of 2");
-	                System.exit(1);
-	            } 
-	        }  
-	        M_2P = M;
-	        N_2P = N;
-	        maxPotM=0;
-	        while ( (1<<maxPotM) <= (M_2P-1) ) {
-	            maxPotM++;
-	        }
-	        maxPotM--;
-	        maxPotN=0;
-	        while ( (1<<maxPotN) <= (N_2P-1) ) {
-	            maxPotN++;
-	        }
-	        maxPotN--;
-	    }
-
-
-	    /** genearl 2D-DFT */
-	    public Complex[][] dft (Complex[][] G) {
-
-	        Complex[][] GF;
-	        Complex gfz;    
-
-	        GF = new Complex[M_2P][N_2P];
-	        gfz = new Complex();
-	        for (int u = 0; u < M_2P; u++) {
-	            for (int v = 0; v < N_2P; v++) {
-	                GF[u][v] = new Complex();
-	                for (int m = 0; m < M_2P; m++) {
-	                    gfz.real = 0.0; gfz.imag = 0.0;
-	                    for (int n = 0; n < N_2P; n++) {
-	                        gfz.add(Complex.mult(G[m][n],
-	                        Complex.expi(-2.0*Math.PI*n*v/N_2P)));
-	                    }           
-	                    (GF[u][v]).add(gfz.mult(Complex.expi(-2.0*Math.PI*m*u/M_2P)));
-	                }
-	                (GF[u][v]).mult(1.0/(M_2P*N_2P));
-	            }
-	        }
-	        return GF;
-	    }
-
-
-	    /** general 2D-IDFT */
-	    public Complex[][] idft (Complex[][] GF) {
-
-	        Complex[][] G;
-	        Complex gz;
-
-	        G = new Complex[M_2P][N_2P];
-	        gz = new Complex();
-	        for (int m = 0; m < M_2P; m++) {
-	            for (int n = 0; n < N_2P; n++) {
-	                G[m][n] = new Complex();
-	                for (int u = 0; u < M_2P; u++) {
-	                    gz.real = 0.0; gz.imag = 0.0;
-	                    for (int v = 0; v < N_2P; v++) {
-	                        gz.add(Complex.mult(GF[u][v], Complex.expi(2.0*Math.PI*n*v/N_2P)));
-	                    }           
-	                    (G[m][n]).add(gz.mult(Complex.expi(2.0*Math.PI*m*u/M_2P)));
-	                }
-	            }
-	        }
-	        return G;
-	    }
-
-
-	    /** 2D-FFT, based on 1D FFT */
-	    public Complex[][] fft (Complex[][] G) {
-
-	        Complex[][] GF; 
-	        Complex[] v,vf;
-	        Fourier1D fft1D;
-
-	        GF = new Complex[M_2P][N_2P];
-
-	        // transform first dimension
-	        fft1D = new Fourier1D(N_2P);
-	        v = new Complex[N_2P]; 
-	        for (int m = 0; m < M_2P; m++) {
-	            for (int n = 0; n < N_2P; n++)
-	                v[n] = new Complex(G[m][n]);
-	            vf = fft1D.fft(v); 
-	            for (int n = 0; n < N_2P; n++)
-	                GF[m][n] = new Complex(vf[n]);
-	        }
-
-	        // transform second dimension
-	        fft1D = new Fourier1D(M_2P);
-	        v = new Complex[M_2P];
-	        for (int n = 0; n < N_2P; n++) {
-	            for (int m = 0; m < M_2P; m++)
-	                v[m] = new Complex(GF[m][n]);
-	            vf = fft1D.fft(v); 
-	            for (int m = 0; m < M_2P; m++)
-	                GF[m][n] = new Complex(vf[m]);
-	        }
-
-	        return GF; 
-	    }   
-
-
-	    /** 2D-IFFT, based on 1D IFFT */
-	    public Complex[][] ifft (Complex[][] G) {
-
-	        Complex[][] GF; 
-	        Complex[] v,vf;
-	        Fourier1D ifft1D;
-
-	        GF = new Complex[M_2P][N_2P];
-
-	        // transform first dimension
-	        ifft1D = new Fourier1D(N_2P);
-	        v = new Complex[N_2P]; 
-	        for (int m = 0; m < M_2P; m++) {
-	            for (int n = 0; n < N_2P; n++)
-	                v[n] = new Complex(G[m][n]);
-	            vf = ifft1D.ifft(v); 
-	            for (int n = 0; n < N_2P; n++)
-	                GF[m][n] = new Complex(vf[n]);
-	        }
-
-	        // transform second dimension
-	        ifft1D = new Fourier1D(M_2P);
-	        v = new Complex[M_2P];
-	        for (int n = 0; n < N_2P; n++) {
-	            for (int m = 0; m < M_2P; m++)
-	                v[m] = new Complex(GF[m][n]);
-	            vf = ifft1D.ifft(v); 
-	            for (int m = 0; m < M_2P; m++)
-	                GF[m][n] = new Complex(vf[m]);
-	        }
-
-	        return GF; 
-	    }   
+				}
+			}
+		}
 	}
-	
-	
-/* void twoDfft(double[][] inputData, double[][] realOut,
- 
-            double[][] complexOut) 
-    {
-        int height = inputData.length;
-        int width = inputData[0].length;
- 
-        // Two outer loops iterate on output data.
-        for (int yWave = 0; yWave < height; yWave++) 
-        {
-            for (int xWave = 0; xWave < width; xWave++) 
-            {
-                // Two inner loops iterate on input data.
-                for (int ySpace = 0; ySpace < height; ySpace++) 
-                {
-                    for (int xSpace = 0; xSpace < width; xSpace++) 
-                    {
-                        // Compute real, imag, and ampltude.
-                        realOut[yWave][xWave] += (inputData[ySpace][xSpace] * Math.cos(2 * Math.PI * ((1.0 * xWave * xSpace / width) + (1.0 * yWave * ySpace / height)))) / Math.sqrt(width * height);
-                        complexOut[yWave][xWave] -= (inputData[ySpace][xSpace] * Math.sin(2 * Math.PI * ((1.0 * xWave * xSpace / width) + (1.0 * yWave * ySpace / height)))) / Math.sqrt(width * height);
-                        /*amplitudeOut[yWave][xWave] = Math.sqrt(realOut[yWave][xWave]* realOut[yWave][xWave] + complexOut[yWave][xWave] * complexOut[yWave][xWave]);*/
-                    /*}
-                    System.out.println(realOut[yWave][xWave] + " + "
-                            + complexOut[yWave][xWave] + " i");
-                }
-            }
-        }
-    }
- 
-    public static void main(String args[]) 
-    {
-        System.out.println("Enter the size: ");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        double[][] input = new double[n][n];
-        double[][] real = new double[n][n];
-        double[][] img = new double[n][n];
-        double[][] amplitutude = new double[n][n];
-        System.out.println("Enter the 2D elements ");
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                input[i][j] = sc.nextDouble();
- 
-        twoDfft(input, real, img, amplitutude);
- 
-        sc.close();
-    }*/
+
+	@SuppressWarnings("null")
+	static void twoDfft(double[][] inputData, double[][] realOut, double[][] complexOut, double[][] amplitudeOut) {
+		int height = inputData.length;
+		int width = inputData[0].length;
+		double[][] real = new double[width][height], complex = new double[width][height];
+
+		// Two outer loops iterate on output data.
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				// Two inner loops iterate on input data.
+				for (int u = 0; u < height; u++) {
+
+					// Compute real, imag, and ampltude.
+					real[x][y] += (inputData[x][u]
+							* Math.sin(2 * Math.PI * ((1.0 * x * u / height))));
+					complex[x][y] -= (inputData[x][u]
+							* Math.cos(2 * Math.PI * ((1.0 * x * u / height))));
+				}
+			}
+		}
+		height = realOut[0].length;
+		width = realOut[0].length;
+		
+		for (int y = 0; y < width; y++) {
+			for (int x = 0; x < height; x++) {
+				// Two inner loops iterate on input data.
+				for (int v = 0; v < height; v++) {
+
+					// Compute real, imag, and ampltude.
+					realOut[x][y] += (real[v][y] * Math.sin(2 * Math.PI * ((1.0 * y * v / width))));
+					complexOut[x][y] -= (complex[v][y] * Math.cos(2 * Math.PI * ((1.0 * y * v / width))));
+				}
+				System.out.print(realOut[x][y]+"\n");
+			}
+		}
+		
+	}
+
+	public void jButton1ActionPerformed(Double[][] img) {
+
+		JLabel imageLabel = new JLabel();
+		JFrame frame = new JFrame();
+		Container contentPane = frame.getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		contentPane.add(new JScrollPane(imageLabel), BorderLayout.CENTER);
+		frame.setSize(600, 400);
+		frame.setVisible(true);
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Enter the size: ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		double[][] input = new double[n][n];
+		double[][] real = new double[n*2][n*2];
+		double[][] img = new double[n*2][n*2];
+		double[][] amplitude = new double[n*2][n*2];
+		System.out.println("Enter the 2D elements ");
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				input[i][j] = sc.nextDouble();
+
+		twoDfft(input, real, img, amplitude);
+
+		sc.close();
+	}
 }
